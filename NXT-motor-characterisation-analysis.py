753e8394464f3c522e499234824a4e388d530ca9
Philip Cutler <cutlervufamily@gmail.com>
2015-05-22 22:20:13 +0930

    fixed time range

--- NXT-motor-characterisation-analysis.py
+++ NXT-motor-characterisation-analysis.py
@@ -15,6 +15,8 @@ power_levels          = np.array(output['power_levels'])
 rhm_angular_positions = np.array(output['rhm_angular_positions'])
 lhm_angular_positions = np.array(output['lhm_angular_positions'])
 
+measurement_times         = measurement_times - measurement_times[0]
+
 rhm_angular_positions     = np.unwrap(np.deg2rad(rhm_angular_positions), np.pi)
 rhm_angular_displacements = np.ediff1d(np.rad2deg(rhm_angular_positions), to_begin=0)
 rhm_linear_displacements  = (rhm_angular_displacements / 360.0) * np.pi * wheel_diameter * 100
@@ -46,13 +48,13 @@ fig, ax1 = plt.subplots()
 ax1.plot(measurement_times, power_levels, 'b-')
 ax1.set_xlabel('Measurement Time (s)')
 # Make the y-axis label and tick labels match the line color.
-ax1.set_ylabel('Power', color='b')
+ax1.set_ylabel('Power Level', color='b')
 for tl in ax1.get_yticklabels():
 	tl.set_color('b')
 
 ax2 = ax1.twinx()
-ax2.plot(measurement_times, rhm_angular_displacements, 'r.')
-ax2.set_ylabel('Angular Displacement', color='r')
+ax2.plot(measurement_times, rhm_angular_displacements, 'r-')
+ax2.set_ylabel('Angular Displacement (degrees)', color='r')
 for tl in ax2.get_yticklabels():
 	tl.set_color('r')
 plt.show()
