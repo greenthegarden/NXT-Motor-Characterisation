6f5ac947d7fde7bfd852c84fde1aac2c9dc955cd
Philip Cutler <cutlervufamily@gmail.com>
2015-06-27 20:42:10 +0930

    new IMU library

---
+++ pyIMU-data-publisher.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from pyberryimu.client import BerryIMUClient
+
+with BerryIMUClient(bus=1) as c :
+	acc  = c.read_accelerometer()
+	gyro = c.read_gyroscope()
+	mag  = c.read_magnetometer()
+	pr   = c.read_pressure()
+	temp = c.read_temperature()
